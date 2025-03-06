d68a3f22c54c1060e61522f7664b27025097abaf
Parissa Chatagny <pchatagn@c3r3s4.42lausanne.ch>
2025-03-06 18:15:01 +0100

    redirection + modification de l'execution

--- src/command/exit.c
+++ src/command/exit.c
@@ -2,15 +2,20 @@
 
 void	exit_command(t_token *lst_token, t_shell *shell, char *input)
 {
-	printf("exit\n");
+	write(STDOUT_FILENO, "exit\n", 5);
 	if (lst_token->next->value)
 	{
 		if (!ft_str_digit(lst_token->next->value))
-			printf("minishell: exit: %s: numeric argument required\n", lst_token->next->value);
+		{
+			write(STDOUT_FILENO, "minishell: exit: ", 17);
+			write(STDOUT_FILENO, lst_token->next->value, ft_strlen(lst_token->next->value));
+			write(STDOUT_FILENO, ": numeric argument required\n", 28);
+		}
 	}
 	free_token_list(lst_token);
 	ft_free_char_tab(shell->var_env);
-	free(input);
+	if (input != NULL)
+		free(input);
 	shell->exit = 0;
 	exit (0);
 }
--- src/execution/builtin_utils.c
+++ src/execution/builtin_utils.c
@@ -1,72 +1,87 @@
 #include "../inc/minishell.h"
 
+//ici il faut gerer: cd, export, unset and exit
+void builtin_parent_process(t_token *lst_token, t_shell *shell, int fd_out)
+{
+	t_token *temp;
 
-//simple redirection au fonctions correpondantes aux buitltin
-// void	exec_builtin_cmd(t_token *lst_token, t_shell *shell, t_file *file)
-// {
-// 	int redir;
+	temp = lst_token;
+	while(temp->type != END)
+	{
+		if (temp->type == WORD)
+		{
+			if (!ft_strncmp(temp->value, "cd", 3))
+				printf("handle cd\n");
+			else if (!ft_strncmp(temp->value, "export", 7))
+				printf("handle export\n");
+			else if (!ft_strncmp(temp->value, "unset", 6))
+				printf("handle unset\n");
+			else if (!ft_strncmp(temp->value, "exit", 5))
+				printf("handle exit\n");
+		}
+		temp = temp->next;
+	}
 
-// 	redir = is_redir(lst_token);
-// 	if (redir)
-// 		printf("redirection need to be handle\n");
-// if (lst_token->type == 0)	
-// 	{
-// 		if (!ft_strncmp(lst_token->value, "env", 4))
-// 			env_command(shell, lst_token, file);
-// 		else if (!ft_strncmp(lst_token->value, "pwd", 4))
-// 			pwd_command(file);
-// 		else if (!ft_strncmp(lst_token->value, "echo", 5))
-// 		{
-// 			if (lst_token->next->type == 8)
-// 				printf("\n");
-// 			else if (!ft_strncmp(lst_token->next->value, "-n", 2))
-// 				echo_command(lst_token->next->next, 1, file);
-// 			else
-// 				echo_command(lst_token->next, 0, file);
-// 		}
-// 		else
-// 		{
-// 			printf("%s: command not found\n", lst_token->value);
-// 			shell->exit = 127;
-// 		}
-// 	}
-// 	if (print_or_file(lst_token))
-// 		fclose(file->file);
-// }
+}
 
+//ici il faut gerer:echo, pwd, env
+//creation d'un child process qui va executer les builtin
+void	builtin_child_process(t_token *lst_token, t_shell *shell, int fd_out)
+{
+	int pid;
+	t_token *temp;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		temp =  lst_token;
+		while (temp->type != END)
+		{
+			 if (!ft_strncmp(temp->value, "env", 4))
+                env_command(shell, lst_token, fd_out);
+            else if (!ft_strncmp(temp->value, "pwd", 4))
+                pwd_command(fd_out);
+            else if (!ft_strncmp(lst_token->value, "echo", 5))
+		// {
+		// 	if (lst_token->next->type == 8)
+		// 		printf("\n");
+		// 	else if (!ft_strncmp(lst_token->next->value, "-n", 2))
+		// 		echo_command(lst_token->next->next, 1, fd_out);
+		// 	else
+		// 		echo_command(lst_token->next, 0, fd_out);
+            temp = temp->next;
+		}
+		exit(0);
+	}
+	else 
+		waitpid(pid, NULL, 0);
+}
+	
 //attribution to the cmd echo, cd, pwd, export, unset, env, exit
 //and if redierction fd_out is beinf passed to the cmd
 //if no redirection fd = -1 
 //builtin qui necessitent stdout: echo, pwd, export, env
 //builtin  qui ncesstient stdin: aucune donc pas besoins de gere < et <<
 //seul truc a gerer est si le file ou directory quil veut ouvrrir n'exsite pas alors rreur
+//pour plus tard -> introduction de pipe
+//run in a forked child: echo, pwd, env
+//run in parent (change de state): cd, export, unset and exit
 void builtin_cmd(t_token *lst_token, t_shell *shell, int fd_out)
 {
 	t_token *temp;
+	int pid;
 
 	temp = lst_token;
 	while (temp->type != END)
 	{
 		if (temp->type == WORD)
 		{
-			if (!ft_strncmp(temp->value, "env", 4))
-			 	env_command(shell, lst_token, fd_out);
-			else if (!ft_strncmp(temp->value, "pwd", 4))
-			 	pwd_command(fd_out);
-			// else if (!ft_strncmp(lst_token->value, "echo", 5))
-			// {
-			// 	if (lst_token->next->type == 8)
-			// 		printf("\n");
-			// 	else if (!ft_strncmp(lst_token->next->value, "-n", 2))
-			// 		echo_command(lst_token->next->next, 1, file);
-			// 	else
-			// 		echo_command(lst_token->next, 0, file);
-			// }
-			// else
-			// {
-			// 	printf("%s: command not found\n", lst_token->value);
-			// 	shell->exit = 127;
-			// }
+			if (!ft_strncmp(temp->value, "cd", 3) || !ft_strncmp(temp->value, "export", 7)
+				|| !ft_strncmp(temp->value, "unset", 6) || !ft_strncmp(temp->value, "exit", 5))
+				builtin_parent_process(lst_token, shell, fd_out);
+			else if (!ft_strncmp(temp->value, "echo", 5) || !ft_strncmp(temp->value, "pwd", 4)
+				|| !ft_strncmp(temp->value, "env", 4))
+				builtin_child_process(lst_token, shell, fd_out);
 		}
 		temp = temp->next;
 	}
