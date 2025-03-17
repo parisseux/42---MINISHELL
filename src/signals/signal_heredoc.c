365bd0c802464c147e1f291be0336f1951d6ca04
Parissa Chatagny <pchatagn@c3r3s4.42lausanne.ch>
2025-03-17 15:35:26 +0100

    minicorrection

--- src/signals/signal_heredoc.c
+++ src/signals/signal_heredoc.c
@@ -3,13 +3,13 @@
 void    sigint_handler_heredoc(int sig)
 {
     (void)sig;
-    //write(STDOUT_FILENO, "\n", 1);
-    // rl_on_new_line();
-	// rl_replace_line("", 0);
-	// rl_redisplay();
-    g_stop = 1;
+    write(STDOUT_FILENO, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+    exit(130);
 }
 void signals_heredoc(void)
 {
+    signal(SIGINT, sigint_handler_heredoc);
 	signal(SIGQUIT, SIG_IGN);
 }
