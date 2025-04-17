c5b6ae8b2cb51edaac31dae742f6ed1af01fb819
Parissa Chatagny <pchatagn@c3r2s4.42lausanne.ch>
2025-04-17 10:33:59 +0200

    norminette unset

--- src/command/unset.c
+++ src/command/unset.c
@@ -15,42 +15,44 @@ int	invalid_option(char *line, t_shell *shell)
 	return (0);
 }
 
-int  remove_var(char **env, char *line)
+int	remove_var(char **env, char *line)
 {
-    int i = 0;
+	int	i;
 
-    while (env[i])
-    {
-        if (!ft_varcmp(env[i], line, ft_strlen(line)))
-        {
-            free(env[i]);
-            while (env[i]) 
-            {
-                env[i] = env[i + 1];
-                i++;
-            }
-            return (1);
-        }
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (env[i])
+	{
+		if (!ft_varcmp(env[i], line, ft_strlen(line)))
+		{
+			free(env[i]);
+			while (env[i])
+			{
+				env[i] = env[i + 1];
+				i++;
+			}
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
+
 int	unset_command(t_token *lst, t_shell *shell)
 {
 	while (lst->type != END && lst->type != PIPE)
 	{
 		if (lst->type == REDIR_IN || lst->type == REDIR_OUT
-            || lst->type == APPEND || lst->type == HEREDOC)
-        {
-            lst = lst->next->next;
-            continue;
-        }
+			|| lst->type == APPEND || lst->type == HEREDOC)
+		{
+			lst = lst->next->next;
+			continue ;
+		}
 		if (lst->type == WORD || lst->type == SQUOTE
 			|| lst->type == DQUOTE || lst->type == BIN || lst->type == DEF)
 		{
 			if (invalid_option(lst->value, shell))
 				return (1);
-			remove_var(shell->var_env,  lst->value);
+			remove_var(shell->var_env, lst->value);
 			if (shell->shell_env)
 				remove_var(shell->shell_env, lst->value);
 		}
@@ -58,4 +60,3 @@ int	unset_command(t_token *lst, t_shell *shell)
 	}
 	return (0);
 }
-
